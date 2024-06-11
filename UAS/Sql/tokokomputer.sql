1f5681f70d3928d755e6bc270a6012ea1b5dbd92
ELY909 <145435556+ELY909@users.noreply.github.com>
2024-06-11 21:46:13 +0800

    Add files via upload
    Nama : Muhammad Arif Rachman|
    Kelas : A 22
    Nim : 2209106009

---
+++ UAS/Sql/tokokomputer.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Jun 2024 pada 15.43
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tokokomputer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akun`
+--
+
+CREATE TABLE `akun` (
+  `id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `password` varchar(11) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `akun`
+--
+
+INSERT INTO `akun` (`id`, `username`, `password`, `isAdmin`) VALUES
+(1, 'admin', 'admin', 1),
+(2, 'arif 1', '123', 0),
+(3, 'arif', '123', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `komputer`
+--
+
+CREATE TABLE `komputer` (
+  `idProduk` varchar(255) NOT NULL,
+  `namaProduk` varchar(255) NOT NULL,
+  `harga` double NOT NULL,
+  `stock` int(11) NOT NULL,
+  `merekProduk` varchar(255) NOT NULL,
+  `Kondisi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `komputer`
+--
+
+INSERT INTO `komputer` (`idProduk`, `namaProduk`, `harga`, `stock`, `merekProduk`, `Kondisi`) VALUES
+('a1', 'Ryzen 3 3200g', 1300000, 5, 'AMD', 'Baru'),
+('a2', 'Vga RX 580', 1000000, 3, 'Nitro Amd', 'Bekas'),
+('a3', 'Cpu i3 12100f', 1500000, 5, 'Intel', 'Baru'),
+('a4', 'Monitor 144Hz', 1800000, 6, 'Viewsonic', 'Baru'),
+('a5', 'Vga Gtx 1660 Ti', 2100000, 3, 'Nvidia Asrock', 'Baru');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderId` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `idProduk` varchar(255) NOT NULL,
+  `orderDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `orders`
+--
+
+INSERT INTO `orders` (`orderId`, `username`, `idProduk`, `orderDate`) VALUES
+(1, 'arif', 'a5', '2024-06-10 16:00:00'),
+(2, 'arif', 'a4', '2024-06-10 16:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `komputer`
+--
+ALTER TABLE `komputer`
+  ADD PRIMARY KEY (`idProduk`);
+
+--
+-- Indeks untuk tabel `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderId`),
+  ADD KEY `username` (`username`),
+  ADD KEY `idProduk` (`idProduk`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
